b5018ef490b52697104ca29d303de556c49db255
chetana <chetu26k@gmail.com>
2018-11-06 11:17:07 +0530

    Fixed Header Style

--- App.js
+++ App.js
@@ -6,19 +6,23 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} ></View> */}
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width:'100%', backgroundColor: 'skyblue'}} >
-            <Image source={require('./img/headerImage.png')} style={{width:'10%', height:'10%'}} />
+          <View style={{width: 50, height: 70}} >
+            <Image source={require('./img/headerImage.png')} style={{width:'80%', height:'50%'}} />
+          </View>
+          <View style={{width: '100%', height: 50}} >
             <Text style = {styles.header}>This is Mobile app</Text>
           </View>
-        </View>     
-        <Image
-          source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}}
-          style={{width: 320, height:180 }}
-        />
+        </View>
+        <View style={{flex: 1, flexDirection: 'column', marginTop: -150}}>
+          <Image
+            source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}}
+            style={{width: 320, height:180 }}
+          />
 
-        <Greeting name='Hi' />
+          <Greeting name='Hi' />
+        </View>        
+       
       </View>
     );
   }
@@ -26,14 +30,13 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     top : '4%' ,
     backgroundColor: '#fff',
   },
   header: {
     fontWeight: 'bold',
     fontSize: 30,
-    backgroundColor: 'rgb(128,128,128)',
     alignItems: 'flex-end',
   }
 });
