eb4837f6bb598244ed4078b8e3a0108296df6fc3
chetana <chetu26k@gmail.com>
2018-11-06 11:30:57 +0530

    Added button and removed bad styles

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {Greeting} from './others/Greeting';
+import {ButtonBasics} from './others/TouchButton';
  
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 0.1, flexDirection: 'row',height: 20,
+          padding: 10,}}>
           <View style={{width: 50, height: 70}} >
             <Image source={require('./img/headerImage.png')} style={{width:'80%', height:'50%'}} />
           </View>
@@ -14,14 +15,8 @@ export default class App extends React.Component {
             <Text style = {styles.header}>This is Mobile app</Text>
           </View>
         </View>
-        <View style={{flex: 1, flexDirection: 'column', marginTop: -150}}>
-          <Image
-            source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}}
-            style={{width: 320, height:180 }}
-          />
-
-          <Greeting name='Hi' />
-        </View>        
+        <ButtonBasics />
+               
        
       </View>
     );
---
+++ others/TouchButton.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+
+export  class ButtonBasics extends Component {
+    _onPressButton() {
+      Alert.alert('You tapped the button!')
+    }
+  
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={this._onPressButton}
+              title="Press Me"
+            />
+          </View>
+          
+          
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     justifyContent: 'center',
+    },
+    buttonContainer: {
+      margin: 20
+    },
+    alternativeLayoutButtonContainer: {
+      margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
+  });
